f61e067007492ddf17ec5b446aaea8ef77bdb842
Bayu Sugiantoro <bayusugiantoro5@gmail.com>
2024-06-19 20:58:08 +0700

    injector parameter

--- test/simple_service_test.go
+++ test/simple_service_test.go
@@ -2,12 +2,19 @@ package test
 
 import (
 	"belajar-golang-restful-api/simple"
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestSimpleService(t *testing.T) {
-	simpleService, err := simple.InitializedService()
-	fmt.Println(err)
-	fmt.Println(simpleService)
+func TestSimpleServiceError(t *testing.T) {
+	simpleService, err := simple.InitializedService(true)
+	assert.NotNil(t, err)
+	assert.Nil(t, simpleService)
+}
+
+func TestSimpleServiceSuccess(t *testing.T) {
+	simpleService, err := simple.InitializedService(false)
+	assert.Nil(t, err)
+	assert.NotNil(t, simpleService)
 }
